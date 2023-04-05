b7a0bc66be69e6d3bd34aad875a11f98b098a046
rodcast <rodcast@gmail.com>
2023-04-05 14:39:46 -0300

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import { Open_Sans } from 'next/font/google';
 import { IMyApp } from '@/interfaces/index';
 import '@/styles/globals.css';
@@ -15,6 +16,17 @@ function MyApp({ Component, pageProps }: IMyApp) {
       <Head>
         <title>{title}</title>
       </Head>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'G-XDP0PEFNH1', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
       <main className={openSans.className}>
         <Component {...pageProps} />
       </main>
