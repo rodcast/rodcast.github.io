3d1261211e02a0ec401035fa9c4e7668e1b9fc1a
rodcast <rodcast@gmail.com>
2023-04-05 19:03:19 -0300

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,15 +16,17 @@ function MyApp({ Component, pageProps }: IMyApp) {
       <Head>
         <title>{title}</title>
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XDP0PEFNH1"
+        strategy="afterInteractive"
+      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-          ga('create', 'G-XDP0PEFNH1', 'auto');
-          ga('send', 'pageview');
+          gtag('config', 'G-XDP0PEFNH1');
         `}
       </Script>
       <main className={openSans.className}>
