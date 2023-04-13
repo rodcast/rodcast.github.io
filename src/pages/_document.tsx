58b69c91778dd6737fccb2f5b9fb8108ab1b2c33
rodcast <rodcast@gmail.com>
2023-04-13 20:59:05 -0300

    feat: update Next.js 13.3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,8 +44,6 @@ const metadata = {
 
 class MyDocument extends Document {
   render() {
-    const { openGraph, twitter, robots } = metadata;
-
     return (
       <Html lang="en" dir="ltr" itemScope itemType="http://schema.org/Person" data-theme="light">
         <Head>
@@ -59,26 +57,26 @@ class MyDocument extends Document {
           <meta name="publisher" content={metadata.publisher} />
           <meta name="format-detection" content={`email=${metadata.formatDetection.email ? 'on' : 'off'}, address=${metadata.formatDetection.address ? 'on' : 'off'}, telephone=${metadata.formatDetection.telephone ? 'on' : 'off'}`} />
 
-          <meta property="og:type" content={openGraph.type} />
-          <meta property="og:title" content={openGraph.title} />
-          <meta property="og:description" content={openGraph.description} />
-          <meta property="og:image" content={openGraph.image} />
-          <meta property="og:url" content={openGraph.url} />
+          <meta property="og:type" content={metadata.openGraph.type} />
+          <meta property="og:title" content={metadata.openGraph.title} />
+          <meta property="og:description" content={metadata.openGraph.description} />
+          <meta property="og:image" content={metadata.openGraph.image} />
+          <meta property="og:url" content={metadata.openGraph.url} />
 
-          <meta name="twitter:title" content={twitter.title} />
-          <meta name="twitter:description" content={twitter.description} />
-          <meta name="twitter:image" content={twitter.image} />
-          <meta name="twitter:site" content={twitter.site} />
-          <meta name="twitter:creator" content={twitter.creator} />
+          <meta name="twitter:title" content={metadata.twitter.title} />
+          <meta name="twitter:description" content={metadata.twitter.description} />
+          <meta name="twitter:image" content={metadata.twitter.image} />
+          <meta name="twitter:site" content={metadata.twitter.site} />
+          <meta name="twitter:creator" content={metadata.twitter.creator} />
 
-          <meta name="robots" content={`${robots.index ? 'index' : 'noindex'}, ${robots.follow ? 'follow' : 'nofollow'}, ${robots.nocache ? 'nocache' : ''}, ${robots.noarchive ? 'noarchive' : ''}`} />
-          {robots.googleBot && (
+          <meta name="robots" content={`${metadata.robots.index ? 'index' : 'noindex'}, ${metadata.robots.follow ? 'follow' : 'nofollow'}, ${metadata.robots.nocache ? 'nocache' : ''}, ${metadata.robots.noarchive ? 'noarchive' : ''}`} />
+          {metadata.robots.googleBot && (
             <>
-              <meta name="googlebot" content={`${robots.googleBot.index ? 'index' : 'noindex'}, ${robots.googleBot.follow ? 'follow' : 'nofollow'}`} />
-              <meta name="googlebot" content={`noimageindex=${robots.googleBot.noimageindex ? 'on' : 'off'}`} />
-              <meta name="googlebot" content={`max-video-preview=${robots.googleBot['max-video-preview']}`} />
-              <meta name="googlebot" content={`max-image-preview=${robots.googleBot['max-image-preview']}`} />
-              <meta name="googlebot" content={`max-snippet=${robots.googleBot['max-snippet']}`} />
+              <meta name="googlebot" content={`${metadata.robots.googleBot.index ? 'index' : 'noindex'}, ${metadata.robots.googleBot.follow ? 'follow' : 'nofollow'}`} />
+              <meta name="googlebot" content={`noimageindex=${metadata.robots.googleBot.noimageindex ? 'on' : 'off'}`} />
+              <meta name="googlebot" content={`max-video-preview=${metadata.robots.googleBot['max-video-preview']}`} />
+              <meta name="googlebot" content={`max-image-preview=${metadata.robots.googleBot['max-image-preview']}`} />
+              <meta name="googlebot" content={`max-snippet=${metadata.robots.googleBot['max-snippet']}`} />
             </>
           )}
           <link rel="icon" href="/favicon.ico" />
