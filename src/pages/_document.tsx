61564dd150db4dda35525857ac226af90cab747a
rodcast <rodcast@gmail.com>
2023-04-14 04:02:38 -0300

    feat: update Next.js 13.3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,8 @@ const metadata = {
 
 class MyDocument extends Document {
   render() {
+    console.log(1);
+    debugger;
     return (
       <Html lang="en" dir="ltr" itemScope itemType="http://schema.org/Person" data-theme="light">
         <Head>
