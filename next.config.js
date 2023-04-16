4c5026657cb130cd4b60d7b52f13732b0adf0c1b
rodcast <rodcast@gmail.com>
2023-04-16 19:49:09 -0300

    feat: performance

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async headers() {
+    return [
+      {
+        // Set cache control for static assets
+        source: "/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=60, immutable",
+          },
+        ],
+      },
+      {
+        // Set cache control for all pages
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
