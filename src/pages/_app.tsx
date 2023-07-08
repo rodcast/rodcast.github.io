5c159a8bd052b0dfe76b223913af8816317eeea7
rodcast <rodcast@gmail.com>
2023-07-08 11:33:08 -0300

    feat: update fonts (Open Sans to Inter)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Script from 'next/script';
-import { Open_Sans } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { IMyApp } from '@/interfaces/index';
 import '@/styles/globals.css';
 
 const title = 'Rodrigo Castilho (RODCAST)';
 
-const openSans = Open_Sans({
+const inter = Inter({
   weight: ['400', '600'],
   subsets: ['latin'],
 });
@@ -30,7 +30,7 @@ function MyApp({ Component, pageProps }: IMyApp) {
           gtag('config', 'G-XDP0PEFNH1');
         `}
       </Script>
-      <main className={openSans.className}>
+      <main className={inter.className}>
         <Component {...pageProps} />
       </main>
     </>
