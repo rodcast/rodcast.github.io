ae19b3ae2695fcd60d024e99b6d8ce0571cf7b1d
rodcast <rodcast@gmail.com>
2023-10-07 13:33:14 -0300

    feat: SSR

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,14 +1,19 @@
 import dynamic from 'next/dynamic';
 import styles from '@/styles/article.module.css';
+import GitHubSkeleton from './GitHubSkeleton';
+import MediumSkeleton from './MediumSkeleton';
 
-const GitHub = dynamic(() => import('./GitHub'));
+const GitHub = dynamic(() => import('./GitHub'), {
+  loading: () => <GitHubSkeleton />,
+});
 
-const Medium = dynamic(() => import('./Medium'));
+const Medium = dynamic(() => import('./Medium'), {
+  loading: () => <MediumSkeleton />,
+});
 
 export default function Article({ dataGitHub, dataMedium }) {
   return (
     <main className={styles.content}>
-      <GitHub data={dataGitHub} />
       <Medium data={dataMedium} />
     </main>
   );
