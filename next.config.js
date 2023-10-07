3aa749c1b7d81fc1177da0ff5f52b143caa956d0
rodcast <rodcast@gmail.com>
2023-10-07 13:01:32 -0300

    feat: SSR

--- next.config.js
+++ next.config.js
@@ -8,37 +8,6 @@ const nextConfig = {
   experimental: {
     largePageDataBytes: 128 * 100000,
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=3600, must-revalidate",
-          },
-        ],
-      },
-      {
-        source: "/_next/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
