f27423755eb869bc9dadf3c576e3d3a8bfcfb916
rodcast <rodcast@gmail.com>
2023-10-07 12:53:48 -0300

    feat: SSR

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: true,
   experimental: {
     largePageDataBytes: 128 * 100000,
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import { NextPageContext } from 'next';
-import dynamic from 'next/dynamic';
 import { fetchData } from '@/utils/fetch';
 import { GITHUB_API, MEDIUM_API } from '@/constants/paths';
 
 import styles from '@/styles/page.module.css';
 
-const Header = dynamic(() => import('@/components/Header'));
+import Header from '@/components/Header';
 
-const Toggle = dynamic(() => import('@/components/Toggle'));
+import Toggle from '@/components/Toggle';
 
-const Sidebar = dynamic(() => import('@/components/Sidebar'));
+import Sidebar from '@/components/Sidebar';
 
-const Article = dynamic(() => import('@/components/Article'));
+import Article from '@/components/Article';
 
 Page.getInitialProps = async (ctx: NextPageContext) => {
   const dataGitHub = await fetchData(GITHUB_API);
