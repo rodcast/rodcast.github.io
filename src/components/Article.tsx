366da71556b7b44e4944db12683fdbb3b12d2e98
Rodrigo Castilho <rodrigo.castilho@gympass.com>
2024-03-30 11:35:49 -0300

    fix: hydration error

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,15 +1,7 @@
-import dynamic from "next/dynamic";
 import styles from "@/styles/article.module.css";
-import GitHubSkeleton from "./GitHubSkeleton";
-import MediumSkeleton from "./MediumSkeleton";
 
-const GitHub = dynamic(() => import("./GitHub"), {
-  loading: () => <GitHubSkeleton />,
-});
-
-const Medium = dynamic(() => import("./Medium"), {
-  loading: () => <MediumSkeleton />,
-});
+import GitHub from "./GitHub";
+import Medium from "./Medium";
 
 export default function Article({ dataGitHub, dataMedium }) {
   return (
